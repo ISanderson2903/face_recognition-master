53bb0c27350bceccce999f5ad3cc09eba6988253
ISanderson2903 <77075536+ISanderson2903@users.noreply.github.com>
2021-01-06 15:05:59 -0500

    wowza man

---
+++ face_recognition-master/CMakeFiles/CMakeProject1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CMakeProject1.dir/CMakeProject1.cpp.obj"
+  "CMakeProject1.exe"
+  "CMakeProject1.exe.manifest"
+  "CMakeProject1.pdb"
+  "libCMakeProject1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CMakeProject1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
